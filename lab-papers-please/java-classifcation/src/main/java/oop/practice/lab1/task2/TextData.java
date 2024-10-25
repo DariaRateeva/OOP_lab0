209734b321c6fb6e8c7840c52f6572c7688af348
dasharateeva <daria.rateeva@isa.utm.md>
2024-10-25 23:53:56 +0300

    Solved task nr 2 -created class FileReader that extracts string from txt file -created class TextData in which I have methods for getting the text, file name, number of vowels, consonance, letters, sentences from the string as well as the longest word. -instances of both classes are initialised in main class where I also print all the information about the string from the txt file -the path for the txt file is given as variable in the args

---
+++ lab-papers-please/java-classifcation/src/main/java/oop/practice/lab1/task2/TextData.java
@@ -0,0 +1,85 @@
+package oop.practice.lab1.task2;
+
+public class TextData {
+    private String fileName;
+    private String text;
+    private int numberOfVowels;
+    private int numberOfConsonants;
+    private int numberOfLetters;
+    private int numberOfSentences;
+    private String longestWord;
+
+    public TextData(String text) {
+        this.text = text;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getNumberOfVowels() {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char c = Character.toLowerCase(text.charAt(i));
+            if(c=='a' || c=='e' || c=='i'||
+                    c=='o' || c=='u'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getNumberOfConsonants() {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char c = Character.toLowerCase(text.charAt(i));
+            if(Character.isLetter(c) && c !='a'&& c!='e'&& c!='i'&&
+                    c!='o' && c!='u'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getNumberOfLetters() {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if(Character.isLetter(text.charAt(i))){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getNumberOfSentences() {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if(text.charAt(i) == '.'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String getLongestWord() {
+        String[] words = text.split("\\W");
+        String longestWord = "";
+        for(String word : words){
+            if(word.length() > longestWord.length()){
+                longestWord = word;
+            }
+        }
+        return longestWord;
+    }
+
+
+}
+
